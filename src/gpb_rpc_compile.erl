550bedb7a8f7a0e29693b8005578f41935907cf4
feng19 <feng_19@foxmail.com>
2016-11-16 10:34:16 +0800

    gen file base on tpl

--- src/gpb_rpc_compile.erl
+++ src/gpb_rpc_compile.erl
@@ -1,231 +1,198 @@
 -module(gpb_rpc_compile).
 -include_lib("gpb/include/gpb.hrl").
 %% API
--export([ file/2 ]).
+-export([ file/4 ]).
 
 -define(EMPTY_LINE, "\n\n").
 
-file(ProtoFile, GpbRpcOpts) ->
+file(ProtoFile, ErlTplFile, HrlTplFile, GpbRpcOpts) ->
     case file:read_file(ProtoFile) of
         {ok, Binary} ->
             String = binary_to_list(Binary),
-            Msg = filename:rootname(filename:basename(ProtoFile)),
-            case parse_lines(Msg, String) of
-                {ok, Result}->
-                    file(Msg, GpbRpcOpts, Result);
+            ProtoName = filename:rootname(filename:basename(ProtoFile)),
+            case parse_lines(ProtoName, String) of
+                {ok, ScanProtoResult}->
+                    file(ProtoName, ErlTplFile, HrlTplFile, GpbRpcOpts, ScanProtoResult);
                 Err -> Err
             end;
         Err -> Err
     end.
 
-file(Msg, GpbRpcOpts, Result) ->
+file(ProtoName, ErlTplFile, HrlTplFile, GpbRpcOpts, ScanProtoResult) ->
     TargetErlDir = proplists:get_value(o_erl, GpbRpcOpts),
     TargetHrlDir = proplists:get_value(o_hrl, GpbRpcOpts),
     ModuleNameSuffix = proplists:get_value(module_name_suffix, GpbRpcOpts),
-    HeaderMsg = proplists:get_value(h_msg, GpbRpcOpts),
     PrefixLen = length(proplists:get_value(msg_prefix, GpbRpcOpts)),
     ModPrefix = proplists:get_value(mod_prefix, GpbRpcOpts),
 
-    ErlTarget = filename:join([TargetErlDir, Msg ++ ".erl"]),
-    HrlTarget = filename:join([TargetHrlDir, Msg ++ ".hrl"]),
+    ErlTarget = filename:join([TargetErlDir, ProtoName ++ ".erl"]),
+    HrlTarget = filename:join([TargetHrlDir, ProtoName ++ ".hrl"]),
 
-    gen_mod(Msg, ModuleNameSuffix, PrefixLen, ModPrefix, HeaderMsg, ErlTarget, Result),
-    gen_hrl(Msg, HrlTarget, Result),
+    gen_mod(ProtoName, ErlTplFile, ModuleNameSuffix, PrefixLen, ModPrefix, ErlTarget, ScanProtoResult),
+    gen_hrl(ProtoName, HrlTplFile, HrlTarget, ScanProtoResult),
     ok.
 
-parse_lines(FName, String) ->
+parse_lines(ProtoName, String) ->
     case gpb_scan:string(String) of
         {ok, Tokens, _} ->
-            case gpb_parse:parse(Tokens++[{'$end',99999}]) of
+            case gpb_parse:parse(Tokens ++ [{'$end', 99999}]) of
                 {ok, Result} ->
                     {ok, Result};
-                {error, {LNum,_Module,EMsg}=Reason} ->
-                    io:format("Parse error on line ~w:~n  ~p~n",
-                        [LNum, {Tokens,EMsg}]),
-                    {error, {parse_error, FName, Reason}}
+                {error, {LNum, _Module, EMsg} = Reason} ->
+                    rebar_api:abort("Parse error on line ~w:~n~p", [LNum, {Tokens, EMsg}]),
+                    {error, {parse_error, ProtoName, Reason}}
             end;
         {error, Reason} ->
-            io:format("Scan error:~n  ~p~n", [Reason]),
-            erlang:error({scan_error, FName, Reason})
+            rebar_api:abort("Scan error:~n~p", [Reason]),
+            erlang:error({scan_error, ProtoName, Reason})
     end.
 
-gen_mod(Msg, ModuleNameSuffix, PrefixLen, ModPrefix, HeaderMsg, Target, Result) ->
-    Service = list_to_atom(Msg++"_service"),
-    case lists:keyfind({service, Service}, 1, Result) of
-        {_, RpcList} ->
-            {CallbackList, Body} = gen_rpc_list(Msg, ModuleNameSuffix, PrefixLen, ModPrefix, HeaderMsg, RpcList),
-            MsgPb = Msg++ModuleNameSuffix,
-            IoData = [
-                gen_header(Msg, MsgPb), ?EMPTY_LINE,
-                CallbackList, ?EMPTY_LINE,
-                Body, ?EMPTY_LINE,
-                gen_encode_msg(MsgPb, HeaderMsg), ?EMPTY_LINE,
-                gen_send_msg(), ?EMPTY_LINE,
-                gen_footer()
-            ],
-            file:write_file(Target, IoData);
-        false -> none
+%%--------------------------------------------------------------------
+gen_mod(ProtoName, ErlTplFile, ModuleNameSuffix, PrefixLen, ModPrefix, Target, ScanProtoResult) ->
+    case file:read_file(ErlTplFile) of
+        {ok, RenderBin} ->
+            Service = list_to_atom(ProtoName ++"_service"),
+            case lists:keyfind({service, Service}, 1, ScanProtoResult) of
+                {_, RpcList} ->
+                    RenderData = gen_mod_do(ProtoName, ModuleNameSuffix, PrefixLen, ModPrefix, RpcList),
+                    IoData = bbmustache:render(RenderBin, RenderData, [{key_type, atom}]),
+                    file:write_file(Target, IoData);
+                false ->
+                    rebar_api:debug("skipped gen gpb rpc : ~p", [ProtoName])
+            end;
+        {error, Reason} ->
+            rebar_api:abort("mustache read file:~p ~p", [ErlTplFile, Reason])
     end.
-
-gen_hrl(Msg0, Target, Result) ->
-    Msg = string:to_upper(Msg0),
-    Body = [ gen_macros(Prefix, EnumList) || {{enum, Prefix}, EnumList} <- Result],
-    IoData = [gen_hrl_header(Msg), Body, gen_hrl_footer()],
-    file:write_file(Target, IoData).
-
-gen_hrl_header(Msg) ->
-"-ifndef("++Msg++"_H).
--define("++Msg++"_H, true).\n".
-
-gen_macros(Prefix, EnumList) ->
-    [gen_macro(Prefix, Key, Value)||{Key, Value} <- EnumList].
-gen_macro(Prefix, Key, Value) ->
-    UpperPrefix = string:to_upper(atom_to_list(Prefix)),
-    UpperKey = string:to_upper(atom_to_list(Key)),
-    Macro = UpperPrefix++"_"++UpperKey,
-    MacroKey = UpperPrefix++"_"++UpperKey++"_KEY",
+gen_mod_do(ProtoName, ModuleNameSuffix, PrefixLen, ModPrefix, RpcList0) ->
+    GpbProto = ProtoName ++ ModuleNameSuffix,
+    HandleMod = ModPrefix++string:substr(ProtoName, PrefixLen+1),
+    ProtoNameUpper = string:to_upper(ProtoName),
+    BaseData = [
+        {proto_name, ProtoName},
+        {proto_name_upper, ProtoNameUpper},
+        {gpb_proto, GpbProto},
+        {handle_mod, HandleMod}
+    ],
+    {CallbackList, RpcList, InputList, OutputList, _} =
+        lists:foldl(
+            fun(Rpc, Acc) ->
+                erl_rpc(Rpc, Acc, BaseData)
+            end, {[], [], [], [], []}, RpcList0),
     [
-        io_lib:format("-define(~ts, ~p).%~p~n", [Macro, Value, Key]),
-        io_lib:format("-define(~ts, ~p).%~p~n", [MacroKey, Key, Value])
+        {proto_name, ProtoName},
+        {proto_name_upper, ProtoNameUpper},
+        {gpb_proto, GpbProto},
+        {handle_mod, HandleMod},
+        {callback_list, CallbackList},
+        {rpc_list, RpcList},
+        {input_list, InputList},
+        {output_list, OutputList}|BaseData
     ].
 
-gen_hrl_footer() ->
-    "\n-endif.".
-
-gen_header(Msg, MsgPb) ->
-    UpperMsg = string:to_upper(Msg),
-"-module("++ Msg ++").
-
--include(\"msg.hrl\").
--ifdef(CMD_"++UpperMsg++").
--include(\""++Msg++".hrl\").
--include(\""++MsgPb++".hrl\").
-
--define(THIS_CMD, ?CMD_"++UpperMsg++").
-
--export([
-    handle_msg/3,
-    decode_input_msg/1,
-    decode_output_msg/1,
-    decode_input/2,
-    decode_output/2,
-    encode_msg/2,
-    send_msg/3
-]).".
-
-gen_rpc_list(Msg, ModuleNameSuffix, PrefixLen, ModPrefix, HeaderMsg, RpcList) ->
-    Mod = ModPrefix++string:substr(Msg, PrefixLen+1),
-    MsgPb = Msg++ModuleNameSuffix,
-    {[CallbackList, HandleMsg, DecodeInput, DecodeOutput], _} =
-        lists:foldl(
-            fun(Rpc, {GenListAcc, OutputList}) ->
-                gen_rpc(MsgPb, HeaderMsg, Mod, Rpc, GenListAcc, OutputList)
-            end, {lists:duplicate(4, []), []}, RpcList),
-    BodyList =
-    [
-        HandleMsg,
-        gen_handle_msg_last(),?EMPTY_LINE,
-        gen_decode_input_msg(),?EMPTY_LINE,
-        gen_decode_output_msg(),?EMPTY_LINE,
-        DecodeInput,
-        gen_decode_input_last(),?EMPTY_LINE,
-        DecodeOutput,
-        gen_decode_output_last(),?EMPTY_LINE
-    ],
-    {CallbackList,BodyList}.
-
-gen_rpc(MsgPb, HeaderMsg, Mod, {Func0, {[Input0], _}, {[Output0], _}, _}, GenListAcc, OutputList) ->
+erl_rpc({Func0, {[undefined], _}, {[Output0], _}, _}, {CallbackList, RpcList, InputList, OutputList, OutputListAcc}, BaseData) ->
     Func = atom_to_list(Func0),
-    Input = atom_to_list(Input0),
-    UpperInput = string:to_upper(Input),
     Output = atom_to_list(Output0),
-    UpperOutput = string:to_upper(Output),
-    {GenList, NewOutputList} =
-    case lists:member(Output0, OutputList) of
+    OutputUpper = string:to_upper(Output),
+    RpcData = [
+        {output, Output},{output_upper, OutputUpper},
+        {handle_func, Func}|BaseData
+    ],
+    case lists:member(Output0, OutputListAcc) of
         true ->
             {
-                [
-                    gen_callback(Func, HeaderMsg),
-                    gen_handle_msg(Mod, Func, MsgPb, Input, UpperInput, UpperOutput),
-                    gen_decode_input(Mod, Func, MsgPb, Input, UpperInput),
-                    ""
-                ],
-                OutputList
+                CallbackList,
+                RpcList,
+                InputList,
+                OutputList,
+                OutputListAcc
             };
         false ->
             {
-                [
-                    gen_callback(Func, HeaderMsg),
-                    gen_handle_msg(Mod, Func, MsgPb, Input, UpperInput, UpperOutput),
-                    gen_decode_input(Mod, Func, MsgPb, Input, UpperInput),
-                    gen_decode_output(MsgPb, Output, UpperOutput)
-                ],
-                [Output0|OutputList]
+                CallbackList,
+                RpcList,
+                InputList,
+                [RpcData|OutputList],
+                [Output0|OutputListAcc]
             }
-    end,
-    NewGenListAcc = lists:zipwith(fun(Gen, OldList) -> [Gen|OldList] end, GenListAcc, GenList),
-    {NewGenListAcc, NewOutputList}.
+    end;
+erl_rpc({Func0, {[Input0], _}, {[undefined], _}, _}, {CallbackList, RpcList, InputList, OutputList, OutputListAcc}, BaseData) ->
+    Func = atom_to_list(Func0),
+    Input = atom_to_list(Input0),
+    InputUpper = string:to_upper(Input),
+    NewCallbackList = [{handle_func, Func}|CallbackList],
+    RpcData = [
+        {input, Input},{input_upper, InputUpper},
+        {handle_func, Func}|BaseData
+    ],
+    {
+        NewCallbackList,
+        RpcList,
+        [RpcData|InputList],
+        OutputList,
+        OutputListAcc
+    };
+erl_rpc({Func0, {[Input0], _}, {[Output0], _}, _}, {CallbackList, RpcList, InputList, OutputList, OutputListAcc}, BaseData) ->
+    Func = atom_to_list(Func0),
+    Input = atom_to_list(Input0),
+    InputUpper = string:to_upper(Input),
+    Output = atom_to_list(Output0),
+    OutputUpper = string:to_upper(Output),
+    HandleFunc = [{handle_func, Func}],
+    NewCallbackList = [HandleFunc|CallbackList],
+    RpcData = [
+        {input, Input},{input_upper, InputUpper},
+        {output, Output},{output_upper, OutputUpper},
+        {handle_func, Func}|BaseData
+    ],
+    case lists:member(Output0, OutputListAcc) of
+        true ->
+            {
+                NewCallbackList,
+                [RpcData|RpcList],
+                InputList,
+                OutputList,
+                OutputListAcc
+            };
+        false ->
+            {
+                NewCallbackList,
+                [RpcData|RpcList],
+                InputList,
+                [RpcData|OutputList],
+                [Output0|OutputListAcc]
+            }
+    end.
 
-gen_callback(Func, HeaderMsg) ->
-"-callback "++Func++"(Msg :: "++HeaderMsg++":msg(), State :: "++HeaderMsg++":state()) -> Reply :: "++HeaderMsg++":func_reply().\n".
-
-gen_handle_msg(_Mod, _Func, _MsgPb, _Input, "UNDEFINED", _UpperOutput) -> "";
-gen_handle_msg(Mod, Func, MsgPb, Input, UpperInput, "UNDEFINED") ->
-"handle_msg(?C_CMD_"++UpperInput++", Binary, State) ->
-    Msg = "++MsgPb++":decode_msg(Binary, "++Input++"),
-    "++Mod++":"++Func++"(Msg, State);\n";
-gen_handle_msg(Mod, Func, MsgPb, Input, UpperInput, UpperOutput) ->
-"handle_msg(?C_CMD_"++UpperInput++", Binary, State) ->
-    Msg = "++MsgPb++":decode_msg(Binary, "++Input++"),
-    Maps = "++Mod++":"++Func++"(Msg, State),
-    case maps:find(msg, Maps) of
-        {ok, RespMsg} ->
-            PbBinary = "++MsgPb++":encode_msg(RespMsg),
-            RespBinary = bg_msg:encode_msg(?THIS_CMD, ?C_CMD_"++UpperOutput++", PbBinary),
-            Maps#{resp_binary => RespBinary};
-        error -> Maps
-    end;\n".
-gen_handle_msg_last() ->
-"handle_msg(CCmd, _Binary, _State) ->
-    #{error => {not_defined_c_cmd, CCmd}}.".
-
-gen_decode_input_msg() ->
-"decode_input_msg(Binary) ->
-    {?THIS_CMD, CCmd, MsgBinary} = bg_msg:decode_msg(Binary),
-    decode_input(CCmd, MsgBinary).".
-
-gen_decode_output_msg() ->
-"decode_output_msg(Binary) ->
-    {?THIS_CMD, CCmd, MsgBinary} = bg_msg:decode_msg(Binary),
-    decode_output(CCmd, MsgBinary).".
-
-gen_decode_input(_Mod, _Func, _MsgPb, _Input, "UNDEFINED") -> "";
-gen_decode_input(Mod, Func, MsgPb, Input, UpperInput) ->
-"decode_input(?C_CMD_"++UpperInput++", MsgBinary) ->
-    {"++Mod++", "++Func++", "++MsgPb++":decode_msg(MsgBinary, "++Input++")};\n".
-gen_decode_input_last() ->
-"decode_input(CCmd, _MsgBinary) ->
-    {error, {not_defined_c_cmd, CCmd}}.".
-
-gen_decode_output(_MsgPb, _Output, "UNDEFINED") -> "";
-gen_decode_output(MsgPb, Output, UpperOutput) ->
-"decode_output(?C_CMD_"++UpperOutput++", MsgBinary) ->
-    "++MsgPb++":decode_msg(MsgBinary, "++Output++");\n".
-gen_decode_output_last() ->
-"decode_output(CCmd, _MsgBinary) ->
-    {error, {not_defined_c_cmd, CCmd}}.".
-
-gen_encode_msg(MsgPb, HeaderMsg) ->
-"encode_msg(CCmd, RespMsg) ->
-    RespBinary = "++MsgPb++":encode_msg(RespMsg),
-    "++HeaderMsg++":encode_msg(?THIS_CMD, CCmd, RespBinary).".
-
-gen_footer() ->
-    "-endif.".
-
-gen_send_msg() ->
-"send_msg(undefined, _CCmd, _Msg) -> undefined;
-send_msg(Pid, CCmd, Msg) ->
-        Binary = encode_msg(CCmd, Msg),
-        Pid ! {send, Binary},
-        ok.".
+%%--------------------------------------------------------------------
+gen_hrl(ProtoName, HrlTplFile, Target, Result) ->
+    case file:read_file(HrlTplFile) of
+        {ok, RenderBin} ->
+            ProtoNameUpper = string:to_upper(ProtoName),
+            BaseData = [
+                {proto_name, ProtoName},
+                {proto_name_upper, ProtoNameUpper}
+            ],
+            EnumsList = [ hrl_enums_list(EnumName, EnumList, BaseData) || {{enum, EnumName}, EnumList} <- Result],
+            RenderData = [{enums_list, EnumsList}|BaseData],
+            IoData = bbmustache:render(RenderBin, RenderData, [{key_type, atom}]),
+            file:write_file(Target, IoData);
+        {error, Reason} ->
+            rebar_api:abort("mustache read file:~p ~p", [HrlTplFile, Reason])
+    end.
+hrl_enums_list(EnumName, EnumList0, BaseData0) ->
+    EnumNameUpper = string:to_upper(atom_to_list(EnumName)),
+    BaseData = [
+        {enum_name, EnumName},
+        {enum_name_upper, EnumNameUpper}|BaseData0
+    ],
+    EnumList = [hrl_enum(EnumKey, EnumValue, BaseData)||{EnumKey, EnumValue} <- EnumList0],
+    [
+        {enum_list, EnumList}|BaseData
+    ].
+hrl_enum(EnumKey, EnumValue, BaseData) ->
+    EnumKeyUpper = string:to_upper(atom_to_list(EnumKey)),
+    [
+        {enum_key, EnumKey},
+        {enum_key_upper, EnumKeyUpper},
+        {enum_value, EnumValue}|BaseData
+    ].
--- src/rebar3_gpb_rpc_compiler.erl
+++ src/rebar3_gpb_rpc_compiler.erl
@@ -1,15 +1,20 @@
 -module(rebar3_gpb_rpc_compiler).
 
--export([compile/1,
-         clean/1]).
+-export([
+    compile/1,
+    clean/1
+]).
 
+-define(OPT_KEYS, [recursive, msg_prefix, mod_prefix, module_name_suffix, o_erl, o_hrl, erl_tpl, hrl_tpl]).
 -define(DEFAULT_PROTO_DIR, "proto").
 -define(DEFAULT_OUT_ERL_DIR, "src/rpc").
 -define(DEFAULT_OUT_HRL_DIR, "include/rpc").
--define(DEFAULT_MODULE_SUFFIX, "").
+-define(DEFAULT_MODULE_SUFFIX, "_pb").
 -define(DEFAULT_HEADER_MSG, "msg").
 -define(DEFAULT_MSG_PREFIX, "msg_").
 -define(DEFAULT_MOD_PREFIX, "mod_").
+-define(DEFAULT_ERL_TPL, "templates/gen_rpc.hrl.tpl").
+-define(DEFAULT_HRL_TPL, "templates/gen_rpc.erl.tpl").
 
 %% ===================================================================
 %% Public API
@@ -18,22 +23,22 @@
 -spec compile(rebar_app_info:t()) -> ok.
 compile(AppInfo) ->
     AppDir = rebar_app_info:dir(AppInfo),
-%%    AppOutDir = rebar_app_info:out_dir(AppInfo),
     Opts = rebar_app_info:opts(AppInfo),
     {ok, GpbOpts} = dict:find(gpb_opts, Opts),
+
     {ok, GpbRpcOpts0} = dict:find(gpb_rpc_opts, Opts),
-    %% check if non-recursive
-    Recursive = proplists:get_value(recursive, GpbRpcOpts0, true),
-    HeaderMsg = proplists:get_value(h_msg, GpbRpcOpts0, ?DEFAULT_HEADER_MSG),
-    MsgPrefix = proplists:get_value(msg_prefix, GpbRpcOpts0, ?DEFAULT_MSG_PREFIX),
-    ModPrefix = proplists:get_value(mod_prefix, GpbRpcOpts0, ?DEFAULT_MOD_PREFIX),
-    ModuleNameSuffix = proplists:get_value(module_name_suffix, GpbOpts,
-                                           ?DEFAULT_MODULE_SUFFIX),
+    GpbRpcOpts = handle_opts(?OPT_KEYS, GpbRpcOpts0),
+
     SourceDirs = proplists:get_all_values(i, GpbOpts),
-    TargetErlDir0 = proplists:get_value(o_erl, GpbRpcOpts0, ?DEFAULT_OUT_ERL_DIR),
+    TargetErlDir0 = proplists:get_value(o_erl, GpbRpcOpts),
     TargetErlDir = filename:join([AppDir, TargetErlDir0]),
-    TargetHrlDir0 = proplists:get_value(o_hrl, GpbRpcOpts0, ?DEFAULT_OUT_HRL_DIR),
+    TargetHrlDir0 = proplists:get_value(o_hrl, GpbRpcOpts),
     TargetHrlDir = filename:join([AppDir, TargetHrlDir0]),
+    ErlTplFile0 = proplists:get_value(erl_tpl, GpbRpcOpts),
+    ErlTplFile = filename:join([AppDir, ErlTplFile0]),
+    HrlTplFile0 = proplists:get_value(hrl_tpl, GpbRpcOpts),
+    HrlTplFile = filename:join([AppDir, HrlTplFile0]),
+
     rebar_api:debug("making sure that target erl dir ~p exists", [TargetErlDir]),
     ok = ensure_dir(TargetErlDir),
     rebar_api:debug("making sure that target hrl dir ~p exists", [TargetHrlDir]),
@@ -41,20 +46,15 @@ compile(AppInfo) ->
     rebar_api:debug("reading proto files from ~p, generating \".erl\" to ~p "
                     "and \".hrl\" to ~p",
       [SourceDirs, TargetErlDir, TargetHrlDir]),
-    %% set the full path for the output directories
-    %% remove the plugin specific options since gpb will not understand them
-    GpbRpcOpts = module_name_suffix_opt(ModuleNameSuffix,
-                msg_prefix_opt(MsgPrefix,
-                    mod_prefix_opt(ModPrefix,
-                        header_msg_opt(HeaderMsg,
-                            target_erl_opt(TargetErlDir,
-                                target_hrl_opt(TargetHrlDir, GpbRpcOpts0)))))),
+
+    %% check if non-recursive
+    Recursive = proplists:get_value(recursive, GpbRpcOpts),
     lists:foreach(fun(SourceDir) ->
                     ok = rebar_base_compiler:run(Opts, [],
                                  filename:join(AppDir, SourceDir), ".proto",
                                  TargetErlDir, ".erl",
                                  fun(Source, Target, Config) ->
-                                    compile(Source, Target, GpbRpcOpts, Config)
+                                    compile(Source, Target, ErlTplFile, HrlTplFile, GpbRpcOpts, Config)
                                  end,
                                  [check_last_mod, {recursive, Recursive}])
                   end, SourceDirs),
@@ -89,11 +89,11 @@ clean(AppInfo) ->
 %% ===================================================================
 %% Private API
 %% ===================================================================
--spec compile(string(), string(), proplists:proplist(), term()) -> ok.
-compile(Source, _Target, GpbRpcOpts, _Config) ->
+-spec compile(string(), string(), string(), string(), proplists:proplist(), term()) -> ok.
+compile(Source, _Target, ErlTplFile, HrlTplFile, GpbRpcOpts, _Config) ->
     rebar_api:debug("compiling ~p", [Source]),
     rebar_api:debug("opts: ~p", [GpbRpcOpts]),
-    case gpb_rpc_compile:file(Source, GpbRpcOpts) of
+    case gpb_rpc_compile:file(Source, ErlTplFile, HrlTplFile, GpbRpcOpts) of
         ok ->
             ok;
         {error, Reason} ->
@@ -112,29 +112,31 @@ ensure_dir(OutDir) ->
     {error, Reason} -> {error, Reason}
   end.
 
--spec target_erl_opt(string(), proplists:proplist()) -> proplists:proplist().
-target_erl_opt(Dir, Opts) ->
-    lists:keystore(o_erl, 1, Opts, {o_erl, Dir}).
+handle_opts([recursive|OptKeys], Opts) ->
+    handle_opts_do(recursive, true, OptKeys, Opts);
+handle_opts([msg_prefix|OptKeys], Opts) ->
+    handle_opts_do(msg_prefix, ?DEFAULT_MSG_PREFIX, OptKeys, Opts);
+handle_opts([mod_prefix|OptKeys], Opts) ->
+    handle_opts_do(mod_prefix, ?DEFAULT_MOD_PREFIX, OptKeys, Opts);
+handle_opts([module_name_suffix|OptKeys], Opts) ->
+    handle_opts_do(module_name_suffix, ?DEFAULT_MODULE_SUFFIX, OptKeys, Opts);
+handle_opts([o_erl|OptKeys], Opts) ->
+    handle_opts_do(o_erl, ?DEFAULT_OUT_ERL_DIR, OptKeys, Opts);
+handle_opts([o_hrl|OptKeys], Opts) ->
+    handle_opts_do(o_hrl, ?DEFAULT_OUT_HRL_DIR, OptKeys, Opts);
+handle_opts([erl_tpl|OptKeys], Opts) ->
+    handle_opts_do(erl_tpl, ?DEFAULT_ERL_TPL, OptKeys, Opts);
+handle_opts([hrl_tpl|OptKeys], Opts) ->
+    handle_opts_do(hrl_tpl, ?DEFAULT_HRL_TPL, OptKeys, Opts);
+handle_opts([], Opts) -> Opts.
 
--spec target_hrl_opt(string(), proplists:proplist()) -> proplists:proplist().
-target_hrl_opt(Dir, Opts) ->
-    lists:keystore(o_hrl, 1, Opts, {o_hrl, Dir}).
-
--spec header_msg_opt(string(), proplists:proplist()) -> proplists:proplist().
-header_msg_opt(HeaderMsg, Opts) ->
-    lists:keystore(h_msg, 1, Opts, {h_msg, HeaderMsg}).
-
--spec msg_prefix_opt(string(), proplists:proplist()) -> proplists:proplist().
-msg_prefix_opt(MsgPrefix, Opts) ->
-    lists:keystore(msg_prefix, 1, Opts, {msg_prefix, MsgPrefix}).
-
--spec mod_prefix_opt(string(), proplists:proplist()) -> proplists:proplist().
-mod_prefix_opt(MsgPrefix, Opts) ->
-    lists:keystore(mod_prefix, 1, Opts, {mod_prefix, MsgPrefix}).
-
--spec module_name_suffix_opt(string(), proplists:proplists()) -> proplists:proplist().
-module_name_suffix_opt(ModuleNameSuffix, Opts) ->
-    lists:keystore(module_name_suffix, 1, Opts, {module_name_suffix, ModuleNameSuffix}).
+handle_opts_do(Key, DefaultValue, OptKeys, Opts) ->
+    case lists:keytake(Key, 1, Opts) of
+        {value, Value, NewOpts} ->
+            handle_opts(OptKeys, lists:keystore(Key, 1, NewOpts, Value));
+        false ->
+            handle_opts(OptKeys, lists:keystore(Key, 1, Opts, {Key, DefaultValue}))
+    end.
 
 find_proto_files(AppDir, GpbOpts) ->
     lists:foldl(fun(SourceDir, Acc) ->
